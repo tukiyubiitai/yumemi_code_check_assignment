05b4b32bedc752462355a8657fedef17903ed5d3
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-11 01:46:22 +0900

    first commit

--- lib/models/repository_detail.dart
+++ lib/models/repository_detail.dart
@@ -8,24 +8,25 @@ part 'repository_detail.g.dart';
 @freezed
 class RepositoryDetail with _$RepositoryDetail {
   const factory RepositoryDetail({
-    required String id,
-    required String name,// リポジトリ名
-    required String language,// プロジェクト言語
-    required int stargazers_count,// star
-    required int watchers_count,// watcher
-    required int forks_count,// fork
-    required int open_issues_count,// issues
+    required int id,
+    @Default('') String name, // リポジトリ名
+    @Default('') String language, // プロジェクト言語
+    required int stargazers_count, // star
+    required int watchers_count, // watcher
+    required int forks_count, // fork
+    required int open_issues_count, // issues
     required Owner owner,
   }) = _RepositoryDetail;
 
-  factory RepositoryDetail.fromJson(Map<String, dynamic> json) => _$RepositoryDetailFromJson(json);
+  factory RepositoryDetail.fromJson(Map<String, dynamic> json) =>
+      _$RepositoryDetailFromJson(json);
 }
 
 @freezed
 class Owner with _$Owner {
-  factory Owner ({
-    @JsonKey(name: 'avatar_url') required String avatarUrl,// オーナーアイコン
-}) =_Owner;
+  factory Owner({
+    @JsonKey(name: 'avatar_url') @Default('') String avatarUrl, // オーナーアイコン
+  }) = _Owner;
 
   factory Owner.fromJson(Map<String, dynamic> json) => _$OwnerFromJson(json);
 }
--- lib/view/search_page.dart
+++ lib/view/search_page.dart
@@ -20,94 +20,128 @@ class _SearchPageState extends ConsumerState<SearchPage> {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _controller,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search_rounded, color: Colors.blue),
-                  border: const OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple.shade300),
-                  ),
-                  labelStyle: const TextStyle(color: Colors.deepPurple)),
-            ),
-          ),
-          Container(
-            height: 50.0,
-            margin: EdgeInsets.all(10),
-            child: ElevatedButton(
-              onPressed: () {
-                ref
-                    .read(searchAsyncNotifierProvider.notifier)
-                    .searchRepositories(_controller.text);
-              },
-              style: ButtonStyle(
-                padding: MaterialStateProperty.all(EdgeInsets.all(0.0)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80.0),
-                  ),
-                ),
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.transparent), // 背景色を透明に
-                // 影を無効にする
-                elevation: MaterialStateProperty.all(0),
-              ),
-              child: Ink(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color(0xff374ABE), Color(0xff64B6FF)],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(30.0)),
-                child: Container(
-                  constraints: BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "検索",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
-                  ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                      prefixIcon:
+                          Icon(Icons.search_rounded, color: Colors.blue),
+                      border: const OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.deepPurple.shade300),
+                      ),
+                      labelStyle: const TextStyle(color: Colors.deepPurple)),
                 ),
               ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: state.when(
-                error: (e, stack) => Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text("見つかりませんでした"),
-                        ],
+              Container(
+                height: 50.0,
+                margin: EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    ref
+                        .read(searchAsyncNotifierProvider.notifier)
+                        .searchRepositories(_controller.text);
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.all(0.0)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
                       ),
                     ),
-                loading: () => Center(
-                      child: CircularProgressIndicator(),
+                    backgroundColor: MaterialStateProperty.all(
+                        Colors.transparent), // 背景色を透明に
+                    // 影を無効にする
+                    elevation: MaterialStateProperty.all(0),
+                  ),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Container(
+                      constraints:
+                          BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "検索",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                data: (data) => ListView.builder(
-                      itemCount: data.repositoryList.length,
-                      itemBuilder: (context, index) {
-                        final repository = data.repositoryList[index];
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(repository.owner.avatarUrl),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: state.when(
+                      error: (e, stack) => Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text("見つかりませんでした"),
+                              ],
+                            ),
                           ),
-                          title: Text(repository.name),
-                          subtitle: Text(repository.language),
-                          trailing: Icon(Icons.arrow_forward),
-                        );
-                      },
-                    )),
+                      loading: () => Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                      data: (data) => ListView.builder(
+                            shrinkWrap: true,
+                            // ListViewが占めるべき高さを自動で計算
+                            physics: NeverScrollableScrollPhysics(),
+                            // ListView内のスクロールを無効化
+                            itemCount: data.repositoryList.length,
+                            itemBuilder: (context, index) {
+                              final repository = data.repositoryList[index];
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.indigo,
+                                  ),
+                                  child: ListTile(
+                                    leading: CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                          repository.owner.avatarUrl),
+                                    ),
+                                    title: Text(
+                                      repository.name,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    subtitle: Text(
+                                      repository.language,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    trailing: Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: Colors.white,
+                                    ),
+                                    onTap: () {},
+                                  ),
+                                ),
+                              );
+                            },
+                          )),
+                ),
+              ),
+            ],
           ),
         ],
       ),
