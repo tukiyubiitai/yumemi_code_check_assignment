339636b93d47e3c2e57de51c62caf5b208166202
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-13 15:02:42 +0900

    feat: リポジトリ詳細画面を実装
    このコミットでは、以下の機能を追加しました。
    
    * リポジトリ詳細画面
    * テーマ切り替えスイッチ
    
    これらの機能により、アプリはユーザーが選択したリポジトリの詳細情報を表示できるようになりました

--- lib/view/repository_detail_page.dart
+++ lib/view/repository_detail_page.dart
@@ -4,18 +4,19 @@ import 'package:yumemi_code_check_assignment/provider/search_notifier.dart';
 
 import '../provider/theme_provider.dart';
 
+/// リポジトリ詳細画面
 class RepositoryDetailPage extends ConsumerWidget {
   const RepositoryDetailPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // 選択されたrepositoryを入れる
+    // 選択されたリポジトリの情報を取得
     final repository = ref.watch(selectedRepositoryProvider);
 
-    // ダークモードの実装
+    // テーマの状態を取得
     final isLightTheme = ref.watch(themeProvider);
 
-    // 再利用されるテキストスタイル
+    // 再利用されるテキストスタイルを定義
     final textStyleWhiteBold =
         TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
     final textStyleWhiteNormal =
@@ -27,6 +28,7 @@ class RepositoryDetailPage extends ConsumerWidget {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.indigo,
         actions: [
+          // テーマ切り替えスイッチ
           Switch(
             value: isLightTheme,
             onChanged: (value) =>
@@ -35,7 +37,8 @@ class RepositoryDetailPage extends ConsumerWidget {
         ],
       ),
       body: repository != null
-          ? Column(
+          ? // リポジトリが取得できた場合の表示
+          Column(
               children: [
                 Container(
                   alignment: Alignment.center,
@@ -50,12 +53,14 @@ class RepositoryDetailPage extends ConsumerWidget {
                     children: [
                       Row(
                         children: [
+                          // リポジトリオーナーのアバター画像
                           CircleAvatar(
                             maxRadius: 38,
                             backgroundImage:
                                 NetworkImage(repository.owner.avatarUrl),
                           ),
                           const SizedBox(width: 20),
+                          // リポジトリ名と言語
                           Expanded(
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
@@ -63,15 +68,16 @@ class RepositoryDetailPage extends ConsumerWidget {
                                 Text(repository.name,
                                     style: textStyleWhiteBold.copyWith(
                                         fontSize: 25),
-                                    maxLines: 2),
+                                    maxLines: 2), // リポジトリ名
                                 const SizedBox(height: 2),
                                 Text(repository.language,
                                     style: textStyleWhiteBold.copyWith(
                                         fontSize: 15),
-                                    maxLines: 1),
+                                    maxLines: 1), // 言語
                               ],
                             ),
                           ),
+                          // 閉じるボタン
                           IconButton(
                             onPressed: () => Navigator.pop(context),
                             icon: const Icon(Icons.close_fullscreen,
@@ -80,6 +86,7 @@ class RepositoryDetailPage extends ConsumerWidget {
                         ],
                       ),
                       const SizedBox(height: 18),
+                      // リポジトリの統計情報
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
