61e6039795925dd3994d9248ab9ec8c8824bc1a0
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-13 14:47:01 +0900

    feat: 検索状態管理機能を追加
    このコミットでは、以下の機能を追加しました。
    
    * SearchStateNotifierクラス: 検索状態を管理するNotifier
    * buildメソッド: 初期状態を設定
    * startSearchingメソッド: 検索開始
    
    これらの機能により、アプリはユーザーが検索を開始したことを検知し、検索結果を表示するまでUIを更新できるようになりました。

--- lib/provider/search_state_provider.dart
+++ lib/provider/search_state_provider.dart
@@ -1,19 +1,28 @@
+// ライブラリのインポート
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+// 自動生成されたファイルのインポート
 part 'search_state_provider.g.dart';
 
+/// 検索状態を管理するNotifierです。
+///
+/// `startSearching` メソッドを使用して検索を開始できます。
+///
+/// 検索中は `true`、それ以外は `false` を返します。
 @riverpod
-class SearchNotifier extends _$SearchNotifier {
+class SearchStateNotifier extends _$SearchStateNotifier {
+  /// 初期状態
+  ///
+  /// 検索中は `true`、それ以外は `false` を返します。
   @override
   bool build() {
+    // 最初は検索していない状態
     return false;
   }
 
+  /// 検索を開始
   void startSearching() {
+    // 検索中状態に更新
     state = true;
-  }
-
-  void endSearching() {
-    state = false;
   }
 }
