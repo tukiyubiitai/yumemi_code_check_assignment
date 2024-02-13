1b9a0a26020029c7474cdf6579bbbc4c4af686df
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-13 12:25:05 +0900

    feat: 検索機能を追加
    このコミットでは、以下の機能を追加しました。
    
    * SearchAsyncNotifier: 検索機能を提供するAsyncNotifier
    * searchReposメソッド: ユーザーが検索を開始するためのメソッド
    * selectedRepositoryプロパティ: 選択されたリポジトリ情報
    * selectedRepository関数: 選択されたリポジトリ情報
    
    これらの機能により、アプリはユーザーが入力したキーワードに基づいてGitHubリポジトリを検索し、選択したリポジトリの詳細情報を表示できるようになりました。

--- lib/provider/search_notifier.dart
+++ lib/provider/search_notifier.dart
@@ -2,10 +2,10 @@ import 'package:logger/logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:yumemi_code_check_assignment/models/repository_state.dart';
 
-import '../key/token.dart';
 import '../models/repository_detail.dart';
 import '../repository/search_reapository.dart';
 
+// 自動生成されたファイルのインポート
 part 'search_notifier.g.dart';
 
 /// 検索機能を提供するAsyncNotifierです。
@@ -16,49 +16,55 @@ part 'search_notifier.g.dart';
 /// 選択されたリポジトリは `selectedRepository` プロパティで取得できます。
 @riverpod
 class SearchAsyncNotifier extends _$SearchAsyncNotifier {
-  SearchRepository get _api => ref.read(searchRepositoryProvider(token));
+  SearchRepository get _api => ref.read(searchRepositoryProvider);
 
+  // ロガーのインスタンス生成
   final logger = Logger();
 
+  /// 初期化処理
+  ///
+  /// 初期状態を設定する。ここでは何もロードしない。
   @override
   Future<RepositoryState> build() async {
-    // 初期状態を設定する。ここでは何もロードしない。
+    // 初期状態を生成
     return RepositoryState(repositoryList: []);
   }
 
   /// ユーザーが検索を開始するためのメソッド
   Future<void> searchRepos(String query) async {
-    // 検索開始を通知し、画面にローディング表示を表示します。
+    // 検索開始を通知
     state = AsyncValue.loading();
 
-    // API通信を行い、検索結果を取得します。
+    // API通信を行い、検索結果を取得
     try {
       final response = await _api.searchRepositories(query);
 
-      // 検索結果を状態に保存し、画面に表示します。
+      // 検索結果を状態に保存
       state = AsyncValue.data(RepositoryState(repositoryList: response));
     } catch (e, stack) {
-      // エラーが発生した場合、エラーログを出力し、エラー状態を画面に表示します。
+      // エラー処理
       logger.e(
           'Error: ${e.toString()}\nStack trace: ${stack.toString().substring(0, 200)}'); // 先頭200文字だけ出力
       state = AsyncValue.error(e, stack); // エラー状態を view に伝える
     }
   }
 
-  /// 選択されたリポジトリを `selectedRepository` に更新します。
+  /// 選択されたリポジトリを更新
   void updateSelectedRepository(RepositoryDetail repository) {
-    // `state.value` をローカル変数 `currentValue` に代入し、null チェック
+    // 現在の状態を取得
     final currentValue = state.value;
+
+    // currentValue が null の場合は何もしない
     if (currentValue == null) {
       return;
     }
 
-    // `currentValue` をコピーし、`selectedRepository` を更新します。
+    // currentValue をコピーし、`selectedRepository` を更新
     final updatedState = currentValue.copyWith(
       selectedRepository: repository,
     );
 
-    // `state` を更新された状態に更新します。
+    // 状態を更新
     state = AsyncValue.data(updatedState);
   }
 }
@@ -70,7 +76,7 @@ RepositoryDetail? selectedRepository(SelectedRepositoryRef ref) {
     // searchAsyncNotifierProviderから現在の状態を取得
     final asyncValue = ref.watch(searchAsyncNotifierProvider).value;
 
-    // asyncValueがnullの場合、何も選択されていないことを意味するので、nullを返す
+    // asyncValue が null の場合は、何も選択されていないことを意味
     if (asyncValue == null) {
       return null;
     }
@@ -78,12 +84,12 @@ RepositoryDetail? selectedRepository(SelectedRepositoryRef ref) {
     // 選択されたリポジトリの情報を取得
     final selectedRepository = asyncValue.selectedRepository;
 
-    // selectedRepositoryがnullの場合、リポジトリが選択されていないか、取得に失敗したことを意味するので、nullを返す
+    // selectedRepository が null の場合は、リポジトリが選択されていないか、取得に失敗
     if (selectedRepository == null) {
       return null;
     }
 
-    // 選択されたリポジトリの詳細情報を持つ新しいRepositoryDetailオブジェクトを作成して返す
+    // 選択されたリポジトリの詳細情報を持つ新しい RepositoryDetail オブジェクトを作成して返す
     return RepositoryDetail(
         id: selectedRepository.id,
         // リポジトリのID
@@ -101,7 +107,7 @@ RepositoryDetail? selectedRepository(SelectedRepositoryRef ref) {
         // オープンされているイシューの数
         owner: selectedRepository.owner); // リポジトリのオーナー情報
   } catch (e) {
-    // 何らかのエラーが発生した場合、エラーログを出力し、例外を再スローする
+    // エラー処理
     logger.e('Error: リポジトリ情報の取得に失敗しました'); // エラーログの出力
     throw 'リポジトリ情報の取得に失敗しました';
   }
