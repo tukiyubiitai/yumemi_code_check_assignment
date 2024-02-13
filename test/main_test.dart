51ec0b14b12931e914a6a40e4a08c9a3307fc0d5
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-13 23:28:21 +0900

    first commit

---
+++ test/main_test.dart
@@ -0,0 +1,6 @@
+import 'package:dio/dio.dart';
+import 'package:mockito/annotations.dart';
+import 'package:yumemi_code_check_assignment/repository/search_repository.dart';
+
+@GenerateNiceMocks([MockSpec<Dio>(), MockSpec<SearchRepository>()])
+main() {}
