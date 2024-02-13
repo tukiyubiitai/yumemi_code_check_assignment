039009998721238d7c4d8c901990d3f8bd190f6c
tukiyubiitai <tukiyubiitai@gmail.com>
2024-02-13 12:18:03 +0900

    feat: SearchRepositoryクラスを追加
    このコミットでは、以下の機能を追加しました。
    
    * SearchRepositoryクラス: GitHub APIとの通信を抽象化するクラス
    * searchRepositoriesメソッド: GitHub APIを使用してリポジトリを検索し、結果をリストで返す
    
    この機能により、アプリはユーザーが入力したキーワードに基づいてGitHubリポジトリを検索できるようになりました。

--- lib/repository/search_reapository.dart
+++ lib/repository/search_reapository.dart
@@ -4,46 +4,60 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'package:yumemi_code_check_assignment/models/repository_detail.dart';
 
-final searchRepositoryProvider = Provider.family<SearchRepository, String>(
-  (_, token) => SearchRepository(),
-);
-
+// ロガーのインスタンス生成
 final logger = Logger();
 
+// SearchRepositoryクラスへのProvider
+final searchRepositoryProvider = Provider<SearchRepository>((ref) {
+  return SearchRepository();
+});
+
+/// GitHub APIとの通信を抽象化するクラスです。
+///
+/// このクラスは、リポジトリの検索機能を提供します。
 class SearchRepository {
-  SearchRepository();
+  /// Dioクライアント
+  final Dio _client;
 
-  // .envからアクセストークンを取得
-  final token = dotenv.env['TOKEN'];
+  /// コンストラクタ
+  ///
+  /// Dioクライアントを生成し、`Authorization` ヘッダーに環境変数から取得した
+  /// アクセストークンを設定します。
+  SearchRepository()
+      : _client = Dio(
+          BaseOptions(
+            baseUrl: "https://api.github.com/",
+            headers: {"Authorization": "Bearer ${dotenv.env['TOKEN']}"},
+          ),
+        );
 
-  //デフォルトはflutter　で検索される
-  String defaultWord = "flutter";
-
-  Dio get _client => Dio(
-        BaseOptions(
-          baseUrl: "https://api.github.com/",
-          headers: {"Authorization": "Bearer $token"},
-        ),
-      );
-
-  Future<List<RepositoryDetail>> getRepositories(
-      {String query = "flutter"}) async {
+  /// GitHub APIを使用して、リポジトリを検索し、結果をリストで返します。
+  ///
+  /// 引数:
+  ///   query: 検索キーワード
+  ///
+  /// 戻り値:
+  ///   検索結果のリポジトリ詳細リスト
+  ///
+  /// エラー発生時:
+  ///   `Future.error` でエラーメッセージを返します。
+  Future<List<RepositoryDetail>> searchRepositories(String query) async {
     try {
-      final result = await _client.get("search/repositories?q=$query");
-      final List<dynamic> items = result.data["items"];
+      // GitHub APIへリクエストを送信し、レスポンスを取得
+      final response = await _client.get("search/repositories?q=$query");
+
+      // レスポンスデータから、リポジトリ情報リストを取得
+      final List<dynamic> items = response.data["items"];
+
+      // 取得したリポジトリ情報リストを、`RepositoryDetail` オブジェクトのリストに変換
       return items
           .map<RepositoryDetail>((itemMap) =>
               RepositoryDetail.fromJson(itemMap as Map<String, dynamic>))
           .toList();
     } catch (e) {
+      // エラーが発生した場合、エラーログを出力し、エラーメッセージを返す
       logger.e('Error: ${e.toString()}');
-      return Future.error("リポジトリの取得に失敗しました: ${e}"); // エラーメッセージを返す
+      return Future.error("リポジトリの取得に失敗しました: ${e}");
     }
-  }
-
-  //検索データ取得処理
-  Future<List<RepositoryDetail>> searchRepositories(String keyWords) async {
-    defaultWord = keyWords; // デフォルトキーワードを書き換え
-    return getRepositories(query: defaultWord); // 検索処理
   }
 }
